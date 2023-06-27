c81aa0eb2f28e45a29eefe4d1bfbd8dd875ead7e
Nilokusumo Tri Pamungkas <44149646+Nktpamungkas@users.noreply.github.com>
2023-06-27 15:30:31 +0700

    balikin ke program mas usman

--- keluar/pages/home.php
+++ keluar/pages/home.php
@@ -27,6 +27,7 @@ include("../koneksi.php");
     }
   </script>
 </head>
+
 <body>
   <?php
     function nourut()
@@ -59,92 +60,21 @@ include("../koneksi.php");
       $idkk = $_GET['idkk'];
     }
 
-    if($_GET['typekk'] == "KKLama"){
-			if ($idkk != "") {
-				date_default_timezone_set('Asia/Jakarta');
-				$qry = mysqli_query($con,"SELECT * FROM tbl_adm WHERE nokk='$idkk' and status='1' and ISNULL(tgl_out) ORDER BY id DESC LIMIT 1");
-				$rw = mysqli_fetch_array($qry);
-				$rc = mysqli_num_rows($qry);
-				$tglsvr = sqlsrv_query($conn,"select CONVERT(VARCHAR(10),GETDATE(),105) AS  tgk");
-				$sr = sqlsrv_fetch_array($tglsvr);
+    if ($idkk != "") {
+      date_default_timezone_set('Asia/Jakarta');
+      $qry1 = mysqli_query($con, "SELECT * FROM tbl_adm WHERE nokk='$idkk' and status='2'  ORDER BY id DESC LIMIT 1");
+      $rw1 = mysqli_fetch_array($qry1);
+      $rc1 = mysqli_num_rows($qry1);
 
-				$sqlLot = sqlsrv_query($conn,"SELECT
-													x.*,dbo.fn_StockMovementDetails_GetTotalWeightPCC(0, x.PCBID) as Weight,
-													dbo.fn_StockMovementDetails_GetTotalRollPCC(0, x.PCBID) as RollCount
-												FROM( SELECT
-															so.CustomerID, so.BuyerID, 
-															sod.ID as SODID, sod.ProductID, sod.UnitID, sod.WeightUnitID, 
-															pcb.ID as PCBID,pcb.UnitID as BatchUnitID,
-															pcblp.DepartmentID,pcb.PCID,pcb.LotNo,pcb.ChildLevel,pcb.RootID
-														FROM
-															SalesOrders so INNER JOIN
-															JobOrders jo ON jo.SOID=so.ID INNER JOIN
-															SODetails sod ON so.ID = sod.SOID INNER JOIN
-															SODetailsAdditional soda ON sod.ID = soda.SODID LEFT JOIN
-															ProcessControlJO pcjo ON sod.ID = pcjo.SODID LEFT JOIN
-															ProcessControlBatches pcb ON pcjo.PCID = pcb.PCID LEFT JOIN
-															ProcessControlBatchesLastPosition pcblp ON pcb.ID = pcblp.PCBID LEFT JOIN
-															ProcessFlowProcessNo pfpn ON pfpn.EntryType = 2 and pcb.ID = pfpn.ParentID AND pfpn.MachineType = 24 LEFT JOIN
-															ProcessFlowDetailsNote pfdn ON pfpn.EntryType = pfdn.EntryType AND pfpn.ID = pfdn.ParentID
-														WHERE pcb.DocumentNo='$idkk' AND pcb.Gross<>'0'
-															GROUP BY
-																so.SONumber, so.SODate, so.CustomerID, so.BuyerID, so.PONumber, so.PODate,jo.DocumentNo,
-																sod.ID, sod.ProductID, sod.Quantity, sod.UnitID, sod.Weight, sod.WeightUnitID,
-																soda.RefNo,pcb.DocumentNo,pcb.Dated,sod.RequiredDate,
-																pcb.ID, pcb.DocumentNo, pcb.Gross,
-																pcb.Quantity, pcb.UnitID, pcb.ScheduledDate, pcb.ProductionScheduledDate,
-																pcblp.DepartmentID,pcb.LotNo,pcb.PCID,pcb.ChildLevel,pcb.RootID
-															) x INNER JOIN
-															ProductMaster pm ON x.ProductID = pm.ID LEFT JOIN
-															Departments dep ON x.DepartmentID  = dep.ID LEFT JOIN
-															Departments pdep ON dep.RootID = pdep.ID LEFT JOIN				
-															Partners cust ON x.CustomerID = cust.ID LEFT JOIN
-															Partners buy ON x.BuyerID = buy.ID LEFT JOIN
-															UnitDescription udq ON x.UnitID = udq.ID LEFT JOIN
-															UnitDescription udw ON x.WeightUnitID = udw.ID LEFT JOIN
-															UnitDescription udb ON x.BatchUnitID = udb.ID
-														ORDER BY
-															x.SODID, x.PCBID ", array(), array( "Scrollable" => 'static' ));
-				$sLot = sqlsrv_fetch_array($sqlLot);
-				$cLot = sqlsrv_num_rows($sqlLot);
-				$child = $sLot['ChildLevel'];
-
-				if ($child > 0) {
-					$sqlgetparent = sqlsrv_query($conn,"select ID,LotNo from ProcessControlBatches where ID='$sLot[RootID]' and ChildLevel='0'");
-					$rowgp = sqlsrv_fetch_array($sqlgetparent);
-
-					//$nomLot=substr("$row2[LotNo]",0,1);
-					$nomLot = $rowgp['LotNo'];
-					$nomorLot = "$nomLot/K$sLot[ChildLevel]&nbsp;";
-				} else {
-					$nomorLot = $sLot['LotNo'];
-				}
-
-				$sqlLot1 = "Select count(*) as TotalLot From ProcessControlBatches where PCID='$sLot[PCID]' and RootID='0' and LotNo < '1000'";
-				$qryLot1 = sqlsrv_query($conn,$sqlLot1) or die('A error occured : ');
-				$rowLot = sqlsrv_fetch_array($qryLot1);
-
-				$sqls = sqlsrv_query($conn,"select processcontrolJO.SODID,salesorders.ponumber,processcontrol.productid,salesorders.customerid,joborders.documentno,
-											salesorders.buyerid,processcontrolbatches.lotno,productcode,productmaster.color,colorno,description,weight,cuttablewidth from Joborders 
-											left join processcontrolJO on processcontrolJO.joid = Joborders.id
-											left join salesorders on soid= salesorders.id
-											left join processcontrol on processcontrolJO.pcid = processcontrol.id
-											left join processcontrolbatches on processcontrolbatches.pcid = processcontrol.id
-											left join productmaster on productmaster.id= processcontrol.productid
-											left join productpartner on productpartner.productid= processcontrol.productid
-											where processcontrolbatches.documentno='$idkk'", array(), array( "Scrollable" => 'static' ));
-				$ssr = sqlsrv_fetch_array($sqls);
-				$cek = sqlsrv_num_rows($sqls);
-				$lgn1 = sqlsrv_query($conn,"select partnername from partners where id='$ssr[customerid]'");
-				$ssr1 = sqlsrv_fetch_array($lgn1);
-				$lgn2 = sqlsrv_query($conn,"select partnername from partners where id='$ssr[buyerid]'");
-				$ssr2 = sqlsrv_fetch_array($lgn2);
-			}
-		}elseif ($_GET['typekk'] == "NOW") {
-			if ($idkk != "") {
-				include_once("../now.php");
-			}
-		}
+      $qry = mysqli_query($con, "SELECT * FROM tbl_adm WHERE nokk='$idkk' and status='1' and ISNULL(tgl_out) ORDER BY id DESC LIMIT 1");
+      $rw = mysqli_fetch_array($qry);
+      $rc = mysqli_num_rows($qry);
+      if ($rc > 0) {
+      } else {
+        echo "<script>alert('Sudah Keluar $rw1[tgl_out] ke $rw1[tujuan]' );</script>";
+      }
+    }
+    //
 
   ?>
 
@@ -168,15 +98,10 @@ include("../koneksi.php");
       mysqli_query($con, $simpanSql) or die("Gagal Ubah" . mysqli_error());
 
       // Refresh form
-      echo "<meta http-equiv='refresh' content='0; url=?idkk=$idkk&status=Data Sudah Keluar'>";
+      echo "<meta http-equiv='refresh' content='0; url=?idkk=$idkk&status=Data Sudah DiUbah'>";
     }
   ?>
   <form id="form1" name="form1" method="post" action="">
-    <?php
-      $q_tbladm   = mysqli_query($con, "SELECT * FROM `tbl_adm` WHERE nokk = '$_GET[idkk]' AND nodemand = '$_GET[demand]'");
-      $d_tbladm   = mysqli_fetch_assoc($q_tbladm);
-    ?>
-    <input type="hidden" value="<?= $d_tbladm['id']; ?>" name="id">
     <table width="100%" border="0">
       <tr>
         <td colspan="6" scope="row">
@@ -211,21 +136,24 @@ include("../koneksi.php");
 					<h4>Nokk</h4>
 				</td>
 				<td width="1%">:</td>
-        <td width="28%">
+				<td width="28%">
 					<input name="nokk" type="text" id="nokk" size="17" onchange="window.location='?typekk='+document.getElementById(`typekk`).value+'&idkk='+this.value" value="<?php echo $_GET['idkk']; ?>" />
+					<input type="hidden" value="<?php echo $rw['id']; ?>" name="id" />
 
-					<?php if ($_GET['typekk'] == 'NOW') { ?>
-					<select style="width: 40%" name="demand" id="demand" onchange="window.location='?typekk='+document.getElementById(`typekk`).value+'&idkk='+document.getElementById(`nokk`).value+'&demand='+this.value" required>
-						<option value="" disabled selected>Pilih Nomor Demand</option>
-						<?php 
-						$sql_ITXVIEWKK_demand  = db2_exec($conn_db2, "SELECT DEAMAND AS DEMAND FROM ITXVIEWKK WHERE PRODUCTIONORDERCODE = '$idkk'");
-						while ($r_demand = db2_fetch_assoc($sql_ITXVIEWKK_demand)) :
-						?>
-						<option value="<?= $r_demand['DEMAND']; ?>" <?php if($r_demand['DEMAND'] == $_GET['demand']){ echo 'SELECTED'; } ?>><?= $r_demand['DEMAND']; ?></option>
-						<?php endwhile; ?>
-					</select>
-					<?php } else { ?>
-						<input name="demand" id="demand" type="text" placeholder="Nomor Demand">
+					<?php if($_GET['typekk'] == 'NOW') { ?>
+						<select style="width: 40%" name="demand" id="demand" onchange='getData_ITXVIEWKK()' required>
+								<?php
+									if($_GET['idkk']) :
+										$qry_demand = db2_exec($conn_db2, "SELECT * FROM ITXVIEWKK WHERE PRODUCTIONORDERCODE LIKE '%$idkk%' AND DEAMAND LIKE '%$nomordemand%'");
+								?>
+								<option value="" disabled selected>Pilih Nomor Demand</option>
+								<?php while ($r_demand = db2_fetch_assoc($qry_demand)) {  ?>
+									<option value="<?= $r_demand['DEAMAND']; ?>" <?php if($_GET['demand'] == $r_demand['DEAMAND']){ echo "SELECTED"; } ?>><?= $r_demand['DEAMAND']; ?></option>
+								<?php } ?>
+								<?php else : ?>
+								<option value="" disabled selected>Masukan Nomor Production Order</option>
+								<?php endif; ?>
+						</select>
 					<?php } ?>
 				</td>
 				<td width="14%">
@@ -252,18 +180,11 @@ include("../koneksi.php");
           <h4>Langganan/Buyer</h4>
         </td>
         <td>:</td>
-        <td>
-          <?php if ($_GET['typekk'] == "NOW") : ?>
-						<?php $langganan_buyer =  $dt_pelanggan_buyer['PELANGGAN'] . '/' . $dt_pelanggan_buyer['BUYER']; ?>
-					<?php else : ?>
-						<?php if ($cek > 0) {
-						$langganan_buyer =  $ssr1['partnername'] . "/" . $ssr2['partnername'];
-						} else {
-						$langganan_buyer =  $rw['langganan'];
-						} ?>
-					<?php endif; ?>
-					<input name="buyer" type="text" id="buyer" size="45" value="<?= $langganan_buyer; ?>"/>
-        </td>
+        <td><input name="buyer" type="text" id="buyer" size="45" value="<?php if ($cek > 0) {
+                                                                          echo $ssr1['partnername'] . "/" . $ssr2['partnername'];
+                                                                        } else {
+                                                                          echo $rw['langganan'];
+                                                                        } ?>" /></td>
         <td><strong>Shift</strong></td>
         <td>:</td>
         <td><select name="shift2" id="shift2" required="required">
@@ -278,20 +199,12 @@ include("../koneksi.php");
           <h4>No. Order</h4>
         </td>
         <td>:</td>
-        <td>
-					<?php if ($_GET['typekk'] == "NOW") : ?>
-						<?php $no_order =  $dt_ITXVIEWKK['PROJECTCODE']; ?>
-					<?php else : ?>
-						<?php if ($cek > 0) {
-						$no_order =  $ssr['documentno'];
-						} else if ($rc > 0) {
-						$no_order =  $rw['no_order'];
-						} else if ($rcAdm > 0) {
-						$no_order = $rwAdm['no_order'];
-						} ?>
-					<?php endif; ?>
-					<input type="text" name="no_order" id="no_order" value="<?= $no_order; ?>" />
-				</td>
+        <td><input type="text" name="no_order" id="no_order" value="<?php if ($cek > 0) {
+                                                                      echo $ssr['documentno'];
+                                                                    } else {
+                                                                      echo $rw['no_order'];
+                                                                    } ?>" />
+        </td>
         <td><strong>Tujuan</strong></td>
         <td>:</td>
         <td><select name="tujuan" id="tujuan" required="required">
@@ -312,20 +225,11 @@ include("../koneksi.php");
           <h4>Jenis Kain</h4>
         </td>
         <td valign="top">:</td>
-        <td>
-          <?php if ($_GET['typekk'] == "NOW") : ?>
-						<?php $jk = $dt_ITXVIEWKK['ITEMDESCRIPTION']; ?>
-					<?php else : ?>
-						<?php if ($cek > 0) {
-						$jk = $ssr['productcode'] . " / " . $ssr['description'];
-						} else if ($rc > 0) {
-						$jk = $rw['jenis_kain'];
-						} else if ($rcAdm > 0) {
-						$jk = $rwAdm['jenis_kain'];
-						} ?>
-					<?php endif; ?>
-					<textarea name="jenis_kain" cols="35" id="jenis_kain"><?= $jk; ?></textarea>
-        </td>
+        <td><textarea name="jenis_kain" cols="35" id="jenis_kain"><?php if ($cek > 0) {
+                                                                    echo $ssr['productcode'] . " / " . $ssr['description'];
+                                                                  } else {
+                                                                    echo $rw['jenis_kain'];
+                                                                  } ?></textarea></td>
         <td valign="top">
           <h4>Catatan</h4>
         </td>
@@ -335,29 +239,20 @@ include("../koneksi.php");
       <tr>
         <td scope="row"><strong>Hanger/Item</strong></td>
         <td>:</td>
-        <td>
-          <?php if ($_GET['typekk'] == "NOW") : ?>
-						<?php $hanger = $dt_ITXVIEWKK['NO_HANGER']; ?>
-					<?php else : ?>
-						<?php if ($cek > 0) {
-							$hanger = $ssr['productcode'];
-						} else if ($rc > 0) {
-							$hanger = $rw['no_item'];
-						} else if ($rcAdm > 0) {
-							$hanger = $rwAdm['no_item'];
-						}?>
-					<?php endif; ?>
-					<input type="text" name="no_item" id="no_item" value="<?= $hanger; ?>" />
-        </td>
+        <td><input type="text" name="no_item" id="no_item" value="<?php if ($cek > 0) {
+                                                                    echo $ssr['productcode'];
+                                                                  } else {
+                                                                    echo $rw['no_item'];
+                                                                  } ?>" /></td>
         <td width="14%"><strong>Lebar X Gramasi</strong></td>
         <td width="1%">:</td>
-        <td><input name="lebar" type="text" id="lebar" size="6" value="<?= floor($dt_lg['LEBAR']); ?><?php if ($cek > 0) {
+        <td><input name="lebar" type="text" id="lebar" size="6" value="<?php if ($cek > 0) {
                                                                           echo $ssr['cuttablewidth'];
                                                                         } else {
                                                                           echo $rw['lebar'];
                                                                         } ?>" placeholder="0" />
           &quot; X
-          <input name="gramasi" type="text" id="gramasi" size="6" value="<?= floor($dt_lg['GRAMASI']); ?><?php if ($cek > 0) {
+          <input name="gramasi" type="text" id="gramasi" size="6" value="<?php if ($cek > 0) {
                                                                             echo $ssr['weight'];
                                                                           } else {
                                                                             echo $rw['gramasi'];
@@ -367,23 +262,14 @@ include("../koneksi.php");
       <tr>
         <td scope="row"><strong>No Warna</strong></td>
         <td>:</td>
-        <td>
-          <?php if ($_GET['typekk'] == "NOW") : ?>
-						<?php $nomor_warna = $dt_ITXVIEWKK['NO_WARNA']; ?>
-					<?php else : ?>
-						<?php if ($cek > 0) {
-							$nomor_warna = $ssr['colorno'];
-						} else if ($rc > 0) {
-							$nomor_warna = $rw['no_warna'];
-						} else if ($rcAdm > 0) {
-							$nomor_warna = $rwAdm['no_warna'];
-						}?>
-					<?php endif; ?>
-					<input name="no_warna" type="text" id="no_warna" size="35" value="<?= $nomor_warna; ?>" />
-        </td>
+        <td><input name="no_warna" type="text" id="no_warna" size="35" value="<?php if ($cek > 0) {
+                                                                                echo $ssr['colorno'];
+                                                                              } else {
+                                                                                echo $rw['no_warna'];
+                                                                              } ?>" /></td>
         <td width="14%"><strong>Berat</strong></td>
         <td width="1%">:</td>
-        <td><input name="qty" type="text" id="qty" size="8" value="<?= $dt_qtyorder['QTY_ORDER']; ?><?php if ($cLot > 0) {
+        <td><input name="qty" type="text" id="qty" size="8" value="<?php if ($cLot > 0) {
                                                                       echo $sLot['Weight'];
                                                                     } else {
                                                                       echo $rw['qty'];
@@ -396,14 +282,14 @@ include("../koneksi.php");
           <h4>Warna</h4>
         </td>
         <td>:</td>
-        <td><input name="warna" type="text" id="warna" size="35" value="<?= $dt_warna['WARNA']; ?><?php if ($cek > 0) {
+        <td><input name="warna" type="text" id="warna" size="35" value="<?php if ($cek > 0) {
                                                                           echo $ssr['color'];
                                                                         } else {
                                                                           echo $rw['warna'];
                                                                         } ?>" /></td>
         <td><strong>Panjang</strong></td>
         <td>:</td>
-        <td><input name="qty2" type="text" id="qty2" size="8" value="<?= $dt_qtyorder['QTY_ORDER_YARD']; ?><?php echo $rw['panjang']; ?>" placeholder="0.00" onFocus="jumlah();" />
+        <td><input name="qty2" type="text" id="qty2" size="8" value="<?php echo $rw['panjang']; ?>" placeholder="0.00" onFocus="jumlah();" />
           <strong>Yard</strong>
         </td>
       </tr>
@@ -422,12 +308,12 @@ include("../koneksi.php");
         </td>
         <td>:</td>
         <td><input name="proses_out" type="text" id="proses_out" placeholder="00:00" pattern="[0-9]{2}:[0-9]{2}$" title=" e.g 14:25 " onkeyup="
-          var time = this.value;
-          if (time.match(/^\d{2}$/) !== null) {
-            this.value = time + ':';
-          } else if (time.match(/^\d{2}\:\d{2}$/) !== null) {
-            this.value = time + '';
-          }" value="<?php echo $rw['jam_out'] ?>" size="5" maxlength="5" required />
+  var time = this.value;
+  if (time.match(/^\d{2}$/) !== null) {
+     this.value = time + ':';
+  } else if (time.match(/^\d{2}\:\d{2}$/) !== null) {
+     this.value = time + '';
+  }" value="<?php echo $rw['jam_out'] ?>" size="5" maxlength="5" required />
           <input name="tgl_proses_k" type="text" id="tgl_proses_k" placeholder="0000-00-00" onclick="if(self.gfPop)gfPop.fPopCalendar(document.form1.tgl_proses_k);return false;" value="<?php echo $rw['tgl_proses_out']; ?>" size="10" required />
           <a href="javascript:void(0)" onclick="if(self.gfPop)gfPop.fPopCalendar(document.form1.tgl_proses_k);return false;"><img src="../calender/calender.jpeg" alt="" name="popcal" width="30" height="25" id="popcal3" style="border:none" align="absmiddle" border="0" /></a>
         </td>
